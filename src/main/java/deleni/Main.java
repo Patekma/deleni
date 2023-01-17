eaa3c5be563b036a87b45f6c17d238437f63abca
Patek <matesptek@seznam.cz>
2023-01-17 13:36:12 +0100

    json generace

--- src/main/java/deleni/Main.java
+++ src/main/java/deleni/Main.java
@@ -1,29 +1,23 @@
 package deleni;
 
+import deleni.gui.MainFrame;
 import deleni.model.PrikladDeleni;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random(System.currentTimeMillis());
-        int max = 100;
-        int pocet = 20;
 
-        List<PrikladDeleni> priklady = new ArrayList<>();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame();
+            }
+        });
 
-        for (int i = 0; i < pocet; i++) {
-            int odmocnina = (int) Math.sqrt(max);
-            int delitel1 = random.nextInt(odmocnina + 1);
-            int delitel2 = random.nextInt(odmocnina) + 1;
-            int delenec = delitel1 * delitel2;
-            priklady.add(new PrikladDeleni(delenec,delitel2));
-        }
 
-        for (PrikladDeleni p : priklady){
-            System.out.println(p);
-        }
     }
 }
--- src/main/java/deleni/gui/MainFrame.java
+++ src/main/java/deleni/gui/MainFrame.java
@@ -1,10 +1,19 @@
 package deleni.gui;
 
+import com.google.gson.Gson;
 import deleni.model.PrikladDeleni;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainFrame extends JFrame {
     private final JTextField tfPocetPrikladu;
@@ -15,8 +24,8 @@ public class MainFrame extends JFrame {
         setTitle("Generovani deleni");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        tfPocetPrikladu = new JTextField(5);
-        tfMax = new JTextField(5);
+        tfPocetPrikladu = new JTextField("20", 5);
+        tfMax = new JTextField("100",5);
 
         JPanel pnlNorth = new JPanel();
         pnlNorth.add(new JLabel("Pocet"));
@@ -26,10 +35,48 @@ public class MainFrame extends JFrame {
 
         JButton btnGeneruj = new JButton("Generuj");
         pnlNorth.add(btnGeneruj);
+        btnGeneruj.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                generuj();
+                ulozDoJsonu();
+            }
+        });
 
         add(pnlNorth, BorderLayout.NORTH);
 
         pack();
         setVisible(true);
+    }
+
+    private void ulozDoJsonu() {
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(priklady));
+        try {
+            PrintStream out = new PrintStream(new FileOutputStream("priklady.json"));
+            out.print(gson.toJson(priklady));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void generuj(){
+        Random random = new Random(System.currentTimeMillis());
+        int max = Integer.valueOf(tfMax.getText());
+        int pocet = Integer.valueOf(tfPocetPrikladu.getText());
+
+        priklady = new ArrayList<>();
+
+        for (int i = 0; i < pocet; i++) {
+            int odmocnina = (int) Math.sqrt(max);
+            int delitel1 = random.nextInt(odmocnina + 1);
+            int delitel2 = random.nextInt(odmocnina) + 1;
+            int delenec = delitel1 * delitel2;
+            priklady.add(new PrikladDeleni(delenec,delitel2));
+        }
+
+        for (PrikladDeleni p : priklady){
+            System.out.println(p);
+        }
     }
 }
